077754c74fc6774838daf80c533294961aa7eb5b
Bohdan Tolmachov <bogdantolmachevvv@gmail.com>
2024-06-20 11:07:10 +0200

    init

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import { ApolloServer } from "@apollo/server"
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import cors from "cors";
+import http from "http";
+import dotenv from "dotenv";
+
+import mergedTypeDefs from "./typeDefs/index.js";
+import mergedResolvers from "./resolvers/index.js";
+
+dotenv.config();
+const app = express();
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+    typeDefs: mergedTypeDefs,
+    resolvers: mergedResolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+await server.start();
+
+app.use(
+    '/',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+        context: async ({ req }) => ({ req }),
+    }),
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
